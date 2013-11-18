3de51909e8dcf019f3715f4f74f3fa51b97ca605
Sefi Ninio <sefi@tikalk.com>
2013-11-18 11:51:08 +0200

    presets

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,37 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MainCtrl', ['$scope','RollDiceService', function($scope, RollDiceService) {
-	$scope.queryString = "";
-	$scope.rolls = [];
-	$scope.resRolls = [];
-	$scope.doRoll = function() {
-		rollerModel = RollDiceService.evaluate($scope.queryString);
-		$scope.rolls = rollerModel.rolls;
-		$scope.resRolls = rollerModel.resRolls;
-		$scope.$apply();
-	}
+	controller('MainCtrl', [
+	  	'$scope',
+	  	'RollDiceService', 
+	  	function($scope, RollDiceService) {
+			$scope.queryString = "";
+			$scope.rolls = [];
+			$scope.resRolls = [];
+			$scope.inputFocus = true;
+			$scope.doRoll = function() {
+				rollerModel = RollDiceService.evaluate($scope.queryString);
+				$scope.rolls = rollerModel.rolls;
+				$scope.resRolls = rollerModel.resRolls;
+				$scope.$apply();
+			}
+			$scope.getPresets = function() {
+				var presets = [];
+				var preset;
 
-  }]);
+				for (var i = 0; i < 20; i++) {
+					preset = localStorage.getItem('preset'+i);
+					if (preset === null) {
+						preset = (i+1)+"d6";
+					}
+					presets.push(preset);
+				}
+
+				return presets;
+			}
+			$scope.handlePreset = function() {
+				$scope.queryString = this.preset;
+				$scope.doRoll();
+			}
+		}
+	]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,18 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  .directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+  })
+;
